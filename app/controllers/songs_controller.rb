f886c0845ee737030293ebb34a735239f5d7fc5b
Adam Duff <j.adam.duff@gmail.com>
2018-09-23 05:02:27 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -15,15 +15,17 @@ class SongsController < ApplicationController
   end
 
   def show
-
+    @song = Song.find(params[:id])
   end
 
   def edit
-
+    @song = Song.find(params[:id])
   end
 
   def update
-
+    @song = Song.find(params[:id])
+    @song.update(name: params[:song][:name], artist_id: params[:song][:artist_id], genre_id: params[:song][:genre_id])
+    redirect_to song_path(@song)
   end
 
 end
