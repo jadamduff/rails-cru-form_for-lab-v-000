1946c4e35cc7c66abc84c7568667c571c121ff80
Adam Duff <j.adam.duff@gmail.com>
2018-09-23 04:55:15 +0000

    automatically backed up by learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,11 +1,16 @@
 class ArtistsController < ApplicationController
 
   def new
-
+    @artist = Artist.new
   end
 
   def create
-
+    @artist = Artist.new
+    @artist.name = params[:artist][:name]
+    @artist.bio = params[:artist][:bio]
+    if @artist.save
+      redirect_to artist_path(@artist)
+    end
   end
 
   def show
