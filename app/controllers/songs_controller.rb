0172686096e59768b64bce59ea7ee0063adcae5f
Adam Duff <j.adam.duff@gmail.com>
2018-09-23 05:00:56 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,11 +1,17 @@
 class SongsController < ApplicationController
 
   def new
-
+    @song = Song.new
   end
 
   def create
-
+    @song = Song.new
+    @song.name = params[:song][:name]
+    @song.artist_id = params[:song][:artist_id]
+    @song.genre_id = params[:song][:genre_id]
+    if @song.save
+      redirect_to song_path(@song)
+    end
   end
 
   def show
