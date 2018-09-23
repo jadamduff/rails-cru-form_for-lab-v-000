cead2c15adb132b1cc98f5fea6853b984076f9e4
Adam Duff <j.adam.duff@gmail.com>
2018-09-23 04:56:21 +0000

    automatically backed up by learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -24,6 +24,7 @@ class ArtistsController < ApplicationController
   def update
     @artist = Artist.find(params[:id])
     @artist.update(name: params[:artist][:name], bio: params[:artist][:bio])
+    redirect_to artist_path(@artist)
   end
 
 end
